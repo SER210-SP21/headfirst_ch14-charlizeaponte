9217260c641adddf9830b35c7e035576ebe96609
charlizeaponte <charlizeaponte@yahoo.com>
2024-04-09 14:01:43 -0400

    Error with gradle flies

---
+++ app/src/main/java/edu/quinnipiac/ser210/task/MainActivity.kt
@@ -0,0 +1,17 @@
+/*
+ @author Charlize Aponte
+ Date: 04/3/24
+ Assignment: Chapter 14 demo
+ */
+package edu.quinnipiac.ser210.task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.quinnipiac.ser210.guessinggame.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
