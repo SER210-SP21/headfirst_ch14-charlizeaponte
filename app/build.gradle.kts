4679d30cddb7176c2c2bdc6d30c24001c2384e95
charlizeaponte <charlizeaponte@yahoo.com>
2024-04-18 14:29:49 -0400

    updated error gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-parcelize")
-    id("kotlin-kapt")
+    id ("com.android.application")
+    id ("kotlin-android")
+    id ("androidx.navigation.safeargs.kotlin")
+    id ("com.google.devtools.ksp") version ("1.8.0-1.0.8")
 }
 android {
     namespace = "edu.quinnipiac.ser210.guessinggame"
@@ -18,7 +18,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
-        dataBinding = true
+        viewBinding= true
     }
 
     buildTypes {
@@ -40,17 +40,22 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation  ("androidx.appcompat:appcompat:$appcompat_version")
+    implementation ("androidx.constraintlayout:constraintlayout:$constraintlayout_version")
+    implementation ("androidx.core:core-ktx:$core_ktx_version")
+    implementation ("com.google.android.material:material: $material_version")
+
+    // Lifecycle libraries
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    // Navigation libraries
+    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Room libraries
     implementation ("androidx.room:room-runtime:$room_version")
+    implementation ("com.android.support:support-annotations:28.0.0")
+    ksp("androidx.room:room-compiler:$room_version")
     implementation ("androidx.room:room-ktx:$room_version")
-    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-    kapt ('androidx.room:room-compiler:$room_version')
-    testImplementation ("junit:junit:4.+")
-    androidTestImplementation ('androidx.test.ext:junit:1.1.3')
-    androidTestImplementation ('androidx.test.espresso:espresso-core:3.4.0')
 }
